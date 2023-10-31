389b8638751bff151d47785820e448bbf389849d
Alfred Umali Paguio <alfredpaguio36@gmail.com>
2023-10-31 21:34:35 +0800

    ESlint and Prettier stuffs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,23 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:import/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
     "plugin:jsx-a11y/recommended",
     "plugin:react-hooks/recommended",
     "prettier",
-    "plugin:prettier-plugin-tailwindcss",
   ],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+      "typescript": {
+        "alwaysTryTypes": true,
+      },
+    }
+  },
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -18,11 +30,12 @@ module.exports = {
     project: ["./tsconfig.json", "./tsconfig.node.json"],
     tsconfigRootDir: __dirname,
   },
-  plugins: ['@typescript-eslint', 'prettier', 'react-refresh'],
+  plugins: ['@typescript-eslint', 'prettier', 'import', 'react-refresh'],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+    "import/no-unresolved": "error"
   },
 };
--- src/pages/Projects/components/ProjectCard.tsx
+++ src/pages/Projects/components/ProjectCard.tsx
@@ -11,7 +11,7 @@ export function ProjectCard({ project }: { project: ProjectType }) {
             <img
               key={index}
               src={imageUrl}
-              alt={`Image ${index}`}
+              alt={`${index}`}
               className="max-w-full"
             />
           ))}
