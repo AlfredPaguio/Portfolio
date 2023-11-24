11996a0d6350e0a47ccb3eb10d761210942d2dae
Alfred Umali Paguio <alfredpaguio36@gmail.com>
2023-11-24 21:35:39 +0800

    Complete sheet implementation

--- src/pages/Projects/layouts/FilterListBar.tsx
+++ src/pages/Projects/layouts/FilterListBar.tsx
@@ -1,23 +1,29 @@
-import { ChevronDown, ChevronUp, FilterIcon, XIcon } from "lucide-react";
+"use client";
+import { FilterIcon, XIcon } from "lucide-react";
 import TechPillButton from "../components/TechPillButton";
 import {
   ACTIONS,
   useTechnologiesContext,
 } from "../contexts/TechnologiesContext";
-import { ReactNode, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
+import { Label } from "@/components/ui/label";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import FilterListMenu from "./FilterListMenu";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
-type FilterListBarProps = {
-  children: ReactNode;
-};
-
-export default function FilterListBar({ children }: FilterListBarProps) {
+export default function FilterListBar() {
   const { selectedTechnologies, dispatch } = useTechnologiesContext();
-
-  const [isExpanded, setIsExpanded] = useState(false);
-  const ButtonIcon = isExpanded ? ChevronUp : ChevronDown;
 
   const removeTechnology = (technology: string) => {
     // Check if the technology is selected
@@ -27,30 +33,60 @@ export default function FilterListBar({ children }: FilterListBarProps) {
     }
   };
 
-  const removeFilters = () => {
+  const removeAllFilters = () => {
     dispatch({ type: ACTIONS.REMOVE_ALL_SELECTED });
   };
 
   return (
     <div className="flex w-full flex-col items-center justify-center gap-y-2 rounded-lg">
-      <Card className="bg-background-light/10 dark:bg-background-dark/10 md:text-md 3xl:text-3xl w-full max-w-7xl px-6 py-3  text-sm  transition-all duration-300 md:flex md:items-center md:px-4 lg:text-lg xl:text-xl 2xl:text-2xl">
-        <Button
-          variant={"ghost"}
-          type="button"
-          onClick={() => setIsExpanded((e) => !e)}
-          className={`order-1 inline-flex shrink-0 items-center p-2 ${
-            isExpanded ? "bg-accent-light/30 dark:bg-accent-dark/30" : ""
-          }`}
-        >
-          <FilterIcon className="mr-2 h-6 w-6" />
-          <span className="font-bold">Filters</span>
-          <ButtonIcon className="ml-2 h-6 w-6" />
+      <Card className="md:text-md 3xl:text-3xl w-full max-w-7xl bg-background-light/10 px-6 py-3 text-sm  transition-all  duration-300 dark:bg-background-dark/10 md:flex md:items-center md:px-4 lg:text-lg xl:text-xl 2xl:text-2xl">
+        <Button variant={"ghost"} type="button" className={`order-1`} asChild>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant={"ghost"}
+                type="button"
+                className={`order-1 inline-flex shrink-0 items-center p-2`}
+              >
+                <FilterIcon className="mr-2 h-6 w-6" />
+                <Label>Filters {selectedTechnologies.length}</Label>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side={"bottom"}>
+              <SheetHeader>
+                <SheetTitle>Filter by Technologies Used</SheetTitle>
+                <SheetDescription>
+                  Find projects based on the technologies used.
+                </SheetDescription>
+                {!!selectedTechnologies.length && (
+                  <Button
+                    type="button"
+                    className="mt-4"
+                    variant={"link"}
+                    onClick={() => removeAllFilters()}
+                  >
+                    <XIcon className="h-6 w-6" /> Clear current filters
+                  </Button>
+                )}
+              </SheetHeader>
+              <ScrollArea>
+                <FilterListMenu />
+              </ScrollArea>
+              <SheetFooter>
+                <SheetClose asChild>
+                  <Button type="button" className="mt-4">
+                    Close
+                  </Button>
+                </SheetClose>
+              </SheetFooter>
+            </SheetContent>
+          </Sheet>
         </Button>
         {!!selectedTechnologies.length && (
           <Button
             variant={"destructive"}
             type="button"
-            onClick={() => removeFilters()}
+            onClick={() => removeAllFilters()}
             className="order-2 ml-2 inline-flex shrink-0 items-center p-2 md:order-4"
           >
             <XIcon className="h-6 w-6" />
@@ -58,7 +94,7 @@ export default function FilterListBar({ children }: FilterListBarProps) {
         )}
         <Separator
           orientation="vertical"
-          className="bg-accent-light dark:bg-accent-dark order-3 hidden h-8 w-0.5 md:order-2 md:mx-4 md:block"
+          className="order-3 hidden h-8 w-0.5 bg-accent-light dark:bg-accent-dark md:order-2 md:mx-4 md:block"
         />
         <div className="order-4 mt-2 flex flex-1 flex-wrap items-center gap-1 md:order-3 md:mt-0">
           {!selectedTechnologies.length && (
@@ -76,8 +112,6 @@ export default function FilterListBar({ children }: FilterListBarProps) {
           ))}
         </div>
       </Card>
-
-      {isExpanded && children}
     </div>
   );
 }
