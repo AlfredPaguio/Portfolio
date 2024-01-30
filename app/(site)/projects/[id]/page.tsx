549154676d65682d8b05c4a537489b7e3cf492d2
Alfred Umali Paguio <alfredpaguio36@gmail.com>
2024-01-30 22:17:58 +0800

    Need Type Safety

--- app/(site)/projects/[id]/page.tsx
+++ app/(site)/projects/[id]/page.tsx
@@ -3,12 +3,12 @@ import { removeFileExtension } from "@/lib/utils";
 // import { MDXRemote } from 'next-mdx-remote/rsc'
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const postData = await fetchContent(params.id);
-  const allContent = await fetchAllContents();
+  const {content}:any = await fetchContent(params.id);
+  // const allContent = await fetchAllContents();
 
   return (
     <div className="wrapper">
-      <h1>{postData?.metadata.title}</h1>
+     {content}
       {/* <MDXRemote {...postData} /> */}
     </div>
   );
