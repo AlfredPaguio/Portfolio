ac97f6503f0c8c32521a388c207e12b5ee2f02ce
AlfredPaguio <alfredpaguio36@gmail.com>
2024-02-04 00:46:37 +0800

    Fix Next.js stuffs

--- src/app/(site)/contact/page.tsx
+++ src/app/(site)/contact/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Facebook, Github, Linkedin, Mail, PhoneIcon, HomeIcon, LanguagesIcon } from "lucide-react";
