75c230dae419ce09e1802818cd27e32d7d623932
AlfredPaguio <alfredpaguio36@gmail.com>
2024-02-08 22:52:34 +0800

    Update Contact page Skeleton

--- src/app/(site)/contact/loading.tsx
+++ src/app/(site)/contact/loading.tsx
@@ -2,6 +2,10 @@ import { Skeleton } from "@@/src/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <Skeleton className="items-center justify-between px-4 pb-4 md:px-16 w-[540px] h-[435px]" />
+    <div className="flex h-full items-center justify-between px-4 pb-4 md:px-16">
+      <div className="flex h-full w-full flex-col items-center justify-center gap-y-4">
+        <Skeleton className="items-center justify-between px-4 pb-4 md:px-16 w-[540px] h-[435px]" />
+      </div>
+    </div>
   );
 }
