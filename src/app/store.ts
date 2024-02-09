4d7fcbfdf7de85c12bffeb034d9c97cb9f21fc4d
AlfredPaguio <alfredpaguio36@gmail.com>
2024-02-09 22:55:41 +0800

    Add Serify-Deserify
    https://www.npmjs.com/package/@karmaniverous/serify-deserify

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,14 @@
 import TechnologyReducer from "../features/technology/technology-slice";
-import { configureStore } from "@reduxjs/toolkit";
+import ProjectReducer from "../features/project/project-slice";
+import { Tuple, configureStore } from "@reduxjs/toolkit";
+import { createReduxMiddleware } from "@karmaniverous/serify-deserify";
+const serifyMiddleware = createReduxMiddleware();
 
 export const store = configureStore({
-  reducer: { technology: TechnologyReducer },
+  reducer: { technology: TechnologyReducer, projects: ProjectReducer },
+  // Add the serify middleware last, or Redux Toolkit's serializableCheck
+  // will reject values before they are serified!
+  middleware: () => new Tuple(serifyMiddleware),
   // middleware: () => new Tuple(additionalMiddleware, logger),
 });
 
