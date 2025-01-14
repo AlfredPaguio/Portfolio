8d540049a06eedbf8ee86ef0fa9c3b0daf58bc15
Alfred Umali Paguio <alfredpaguio36@gmail.com>
2025-01-14 13:37:55 +0800

    Remove Keystatic Cloud (#54)

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,24 +1,31 @@
+import { siteConfig } from "@/config/site";
 import { REPOSITORIES, REPO_OWNER } from "@/data/Repositories";
-import { CloudConfig, GitHubConfig, LocalConfig, config } from "@keystatic/core";
+import {
+  CloudConfig,
+  GitHubConfig,
+  LocalConfig,
+  config,
+} from "@keystatic/core";
+import { aboutSchema } from "./schema/about";
+import { linksSchema } from "./schema/links";
 import { projectsSchema } from "./schema/projects";
 import { techStackSchema } from "./schema/techStack";
-import { linksSchema } from "./schema/links";
-import { aboutSchema } from "./schema/about";
-import { siteConfig } from "@/config/site";
 
 //https://vercel.com/docs/projects/environment-variables/system-environment-variables
-const isProd = process.env.NEXT_PUBLIC_VERCEL_ENV === "production" || false;
-const storage: LocalConfig["storage"] | CloudConfig["storage"] = isProd
-  ? {
-      kind: "cloud",
-    }
-  : { kind: "local" };
+// const isProd = process.env.NEXT_PUBLIC_VERCEL_ENV === "production" || false;
+// const storage: LocalConfig["storage"] | CloudConfig["storage"] = isProd
+//   ? {
+//       kind: "cloud",
+//     }
+//   : { kind: "local" };
+
+const storage: LocalConfig["storage"] = { kind: "local" };
 
 export default config({
   storage,
-  cloud: {
-    project: 'alfred-paguio/portfolio',
-  },
+  // cloud: {
+  //   project: "alfred-paguio/portfolio",
+  // },
   ui: {
     brand: { name: siteConfig.name },
     navigation: {
