49d03e7effe454a356d08f4d2542f956ea945bfa
Alfred Umali Paguio <alfredpaguio36@gmail.com>
2025-01-18 09:58:32 +0800

    Update some outdated packages

--- src/app/(site)/Providers.tsx
+++ src/app/(site)/Providers.tsx
@@ -3,11 +3,13 @@
 import { ReactNode } from "react";
 import ReduxProvider from "../store/ReduxProvider";
 import { ThemeProvider } from "@/components/theme-provider";
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
 import NavigationHandler from "@/components/NavigationHandler";
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <ReduxProvider>
+      <NuqsAdapter>
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
@@ -17,6 +19,7 @@ export default function Providers({ children }: { children: ReactNode }) {
         {children}
         {/* <NavigationHandler />  */}
       </ThemeProvider>
+      </NuqsAdapter>
     </ReduxProvider>
   );
 }
