ec9f4f1b86bdde5abd9c5bc652c97ab1796a7d9f
AlfredPaguio <alfredpaguio36@gmail.com>
2025-02-12 17:52:41 +0800

    Update Icons.tsx
    Add shadcn/ui logo

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -51,6 +51,37 @@ const externalIcons: Record<string, string | JSX.Element> = {
     </svg>
   ),
   "beautiful soup": "https://www.crummy.com/software/BeautifulSoup/10.1.jpg",
+  "shadcn/ui": (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      className="size-4"
+    >
+      <rect width="256" height="256" fill="none"></rect>
+      <line
+        x1="208"
+        y1="128"
+        x2="128"
+        y2="208"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="32"
+      ></line>
+      <line
+        x1="192"
+        y1="40"
+        x2="40"
+        y2="192"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="32"
+      ></line>
+    </svg>
+  ),
 };
 
 // Cache for dynamic imports
